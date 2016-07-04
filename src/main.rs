044ef2a5ff53f0dc71eca891365f7c6109dec5b5
decentninja <very@decent.ninja>
2016-07-05 00:00:17 +0900

    Don't know if working bug should update to see if segfault is solved

--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,94 @@
 extern crate portmidi as pm;
-
 use std::thread;
+use std::collections::HashSet;
 use std::time::Duration;
 
-static CHANNEL: u8 = 0;
-
-struct Cheatstrument {
-    output_port: pm::OutputPort,
-    CHANNEL: u8,
-    device_recives: bool,
-    down: Vec<u8>
+#[derive(Debug)]
+enum MidiError {
+    SendError(pm::Error)
 }
 
-impl Drop for Cheatstrument {
-    fn drop(&mut self) {
-        let mut clone = self.down.clone();
-        for midi in clone {
-            self.up(midi);
+trait MidiWrapper {
+    fn down(&mut self, note: u8, velocity: u8) -> Result<(), MidiError>;
+    fn up(&mut self, note: u8) -> Result<(), MidiError>;
+    fn get_down(&self) -> &HashSet<u8>;
+}
+
+struct PortMidiWrapper {
+    output_port: pm::OutputPort,
+    channel: u8,
+    down: HashSet<u8>,
+}
+
+impl PortMidiWrapper {
+    pub fn new() -> Result<Self, pm::Error> {
+        let context = try!(pm::PortMidi::new());
+        let device = try!(context.device(1));
+        let output_port = try!(context.output_port(device, 1024));
+        Ok(PortMidiWrapper {
+            output_port: output_port,
+            channel: 1,
+            down: HashSet::new()
+        })
+    }
+
+    fn send(&mut self, note: u8, velocity: u8, isdown: bool) -> Result<(), MidiError> {
+        let status = if isdown { 0x90 } else { 0x80 };
+        let message = pm::MidiMessage {
+            status: status + self.channel,
+            data1: note,
+            data2: velocity // TODO: Check if velocity
+        };
+        match self.output_port.write_message(message) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(MidiError::SendError(e)),
         }
     }
 }
 
-impl Cheatstrument {
-
-    pub fn new() -> Result<Self, pm::Error> {
-        // TODO: Wrap error handling by replacing pm::Error by new CheatError that implements FROMs.
-        let context = try!(pm::PortMidi::new());
-        let device = try!(context.device(1));
-        let output_port = try!(context.output_port(device, 1024));
-        Ok(Cheatstrument {
-            output_port: output_port,
-            CHANNEL: CHANNEL,
-            device_recives: true,
-            down: vec!()
-        })
+impl MidiWrapper for PortMidiWrapper {
+    fn down(&mut self, note: u8, velocity: u8) -> Result<(), MidiError> {
+        self.down.insert(note);
+        self.send(note, velocity, true)
     }
 
-    fn send(&mut self, midi: u8, status: u8) {
-        let message = pm::MidiMessage {
-            status: status + self.CHANNEL,
-            data1: midi,
-            data2: 100, // Velocity?
-        };
-        let result = self.output_port.write_message(message);
-        self.device_recives = result.is_ok();
+    fn up(&mut self, note: u8) -> Result<(), MidiError> {
+        self.down.remove(&note);
+        self.send(note, 0, false)
     }
 
-    fn down(&mut self, midi: u8) {
-        self.send(midi, 0x90);
+    fn get_down(&self) -> &HashSet<u8> {
+        &self.down
     }
+}
 
-    fn up(&mut self, midi: u8) {
-        self.send(midi, 0x80);
+impl Drop for PortMidiWrapper {
+    fn drop(&mut self) {
+        for note in self.down.clone() {
+            let _ = self.send(note.clone(), 0, false);
+        }
     }
+}
 
+pub struct Cheatstrument {
+    midi: MidiWrapper
 }
 
 #[test]
-fn portmidi_test() {
-    let context = pm::PortMidi::new().expect("Could not connect to PortMidi.");
-    assert!(true);
-    let mut out_port = context.device(1)
-                          .and_then(|dev| context.output_port(dev, 1024))
-                          .expect("Could not connect to device");
-    let note_on = pm::MidiMessage {
-        status: 0x90 + CHANNEL,
-        data1: 60,
-        data2: 100,
-    };
-    out_port.write_message(note_on).expect("Could not send message");
+fn portmidiwrapper_test() {
+    let mut midi = PortMidiWrapper::new().unwrap();
+    midi.down(60, 100).unwrap();
     thread::sleep(Duration::from_millis(400));
-    let note_off = pm::MidiMessage {
-        status: 0x80 + CHANNEL,
-        data1: 60,
-        data2: 100,
-    };
-    out_port.write_message(note_off).expect("Could not send message");
+    midi.up(60).unwrap();
+}
+
+#[test]
+fn portmidi_drop_test() {
+    let mut midi = PortMidiWrapper::new().unwrap();
+    midi.down(70, 100).unwrap();
+    thread::sleep(Duration::from_millis(400));
+    // Should end tone by themselvs
 }
 
 fn main() {
-    Cheatstrument::new();
 }
